0e169b5dfa2f472dd1ede9257cae5a7508391281
Rajesh Ramsaroop <rramsaro@ggc.edu>
2012-10-27 11:24:51 -0400

    sdfgsdfg

--- src/itec2150project/MediaConsumptionTracker.java
+++ src/itec2150project/MediaConsumptionTracker.java
@@ -2,15 +2,18 @@ package itec2150project;
 
 import java.awt.BorderLayout;
 import java.awt.CardLayout;
+import java.awt.Color;
+import java.awt.Font;
 import java.awt.GridLayout;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
-import javax.swing.JTextField;
+import javax.swing.JScrollPane;
+import javax.swing.JTable;
 
 /**
- * @author Raj Ramsaroop, Greg Westerfield, Jr.
+ * @author Raj Ramsaroop
  * @version 0.1
  */
 public class MediaConsumptionTracker {
@@ -20,21 +23,21 @@ public class MediaConsumptionTracker {
 	
 	// Main Menu Panel
 	JPanel pnlMainMenu;
-
+	
 	// Main menu buttons
 	JButton btnMainMusic, btnMainMovies, btnMainBooks;
 	
-	//Add music panel
-	JPanel pnlAddMusic;
+	// Panel to contain Manage Music screen
+	JPanel pnlManageMusic;
 	
-	//Add music buttons
-	JButton btnAddMusic, btnCancel;
+	// Label for Manage Music screen title
+	JLabel lblManageMusic;
 	
-	//Add music labels
-	JLabel lblAddMusic, lblSongTitle, lblArtist, lblAlbum;
+	// Button to add music on Manage Music screen
+	JButton btnAddMusic;
 	
-	//Add music text fields 
-	JTextField txtSongTitle, txtArtist, txtAlbum;
+	// Manage music table
+	JTable tblManageMusic;
 	
 	public MediaConsumptionTracker() {
 		
@@ -49,28 +52,57 @@ public class MediaConsumptionTracker {
 		
 		// Create main menu panel
 		createMainMenu();
-		// Create add music panel
-		createAddMusicScreen();
+		
+		// Create Manage Music screen
+		createManageMusicScreen();
+		
 		// Create and initialize frame, make main menu the first panel displayed
 		frame = new JFrame("Media Consumption Tracker");
 		frame.setSize(450,470);
 		frame.setLayout(new CardLayout());
-		frame.add(pnlAddMusic, "Add Music");
-		frame.add(pnlMainMenu,"Main Menu");
+		//frame.add(pnlMainMenu,"Main Menu");
+		frame.add(pnlManageMusic,"Manage Music");
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.setVisible(true);
 	}
-	/**
-	 * Creates the add music screen components
-	 */
-	private void createAddMusicScreen() {
-		//Create main panel 
-		pnlAddMusic = new JPanel();
-		pnlAddMusic.setLayout(new BorderLayout());
-		pnlAddMusic.add(lblAddMusic = new JLabel("Add Music"));
+
+	private void createManageMusicScreen() {
+		lblManageMusic = new JLabel("Manage Music");
+		lblManageMusic.setFont(new Font("Comic Sans MS", Font.BOLD , 22));
+		lblManageMusic.setForeground(Color.MAGENTA);
 		
-		//pnlAddMusic.add(lblAddMusic = new JLabel("Add Music"));
+		// Add Music button
+		btnAddMusic = new JButton("Add Music");
 		
+		// Create headings
+		String[] columnNames = {"Song Title",
+								"Artist",
+								"Album"};
+		
+		// Create sample data for testing, this will be pulled from the database in the real
+		// program
+		Object[][] sampleData = {
+			    {"Ssdfome Title", "Crappy Artist", "Stupid eeeAlbum"},
+			    {"Some Title", "Czxczxcvrappy Artist", "Stupvvixcvzxcvd Album"},
+			    {"Somebn Title", "Crapzxcvzxcpy Artist", "Stupidxcvzxcv Album"},
+			    {"Somevzxcvx Title", "Crappy 334Artist", "Stupid Album"},
+			    {"Somebvn Title", "Cdffrappy Artist", "Stupid vbnbAlbum"}
+		};
+		
+		JTable tblManageMusic = new JTable(sampleData, columnNames);
+		tblManageMusic.setEnabled(false);	// Disable editing for now
+		tblManageMusic.setAutoCreateRowSorter(true);
+		
+		// Create scroll pane to contain table
+		JScrollPane scrollPane = new JScrollPane(tblManageMusic);
+		tblManageMusic.setFillsViewportHeight(true);
+		
+		// Create panel to contain everything
+		pnlManageMusic = new JPanel();
+		pnlManageMusic.setLayout(new GridLayout(3,1));
+		pnlManageMusic.add(lblManageMusic);
+		pnlManageMusic.add(btnAddMusic);
+		pnlManageMusic.add(scrollPane);
 		
 	}
 
