1d151588389ee2dcaa45bb0883645217b6901406
Rajesh Ramsaroop <rramsaro@ggc.edu>
2012-10-27 11:21:22 -0400

    Revert "Sync Test"
    This reverts commit 44d75e4580a05be46c91a2a29746c1bb4497ebf0.

--- src/itec2150project/MediaConsumptionTracker.java
+++ src/itec2150project/MediaConsumptionTracker.java
@@ -2,18 +2,15 @@ package itec2150project;
 
 import java.awt.BorderLayout;
 import java.awt.CardLayout;
-import java.awt.Color;
-import java.awt.Font;
 import java.awt.GridLayout;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
-import javax.swing.JScrollPane;
-import javax.swing.JTable;
+import javax.swing.JTextField;
 
 /**
- * @author Raj Ramsaroop
+ * @author Raj Ramsaroop, Greg Westerfield, Jr.
  * @version 0.1
  */
 public class MediaConsumptionTracker {
@@ -23,21 +20,21 @@ public class MediaConsumptionTracker {
 	
 	// Main Menu Panel
 	JPanel pnlMainMenu;
-	
+
 	// Main menu buttons
 	JButton btnMainMusic, btnMainMovies, btnMainBooks;
 	
-	// Panel to contain Manage Music screen
-	JPanel pnlManageMusic;
+	//Add music panel
+	JPanel pnlAddMusic;
 	
-	// Label for Manage Music screen title
-	JLabel lblManageMusic;
+	//Add music buttons
+	JButton btnAddMusic, btnCancel;
 	
-	// Button to add music on Manage Music screen
-	JButton btnAddMusic;
+	//Add music labels
+	JLabel lblAddMusic, lblSongTitle, lblArtist, lblAlbum;
 	
-	// Manage music table
-	JTable tblManageMusic;
+	//Add music text fields 
+	JTextField txtSongTitle, txtArtist, txtAlbum;
 	
 	public MediaConsumptionTracker() {
 		
@@ -52,57 +49,28 @@ public class MediaConsumptionTracker {
 		
 		// Create main menu panel
 		createMainMenu();
-		
-		// Create Manage Music screen
-		createManageMusicScreen();
-		
+		// Create add music panel
+		createAddMusicScreen();
 		// Create and initialize frame, make main menu the first panel displayed
 		frame = new JFrame("Media Consumption Tracker");
 		frame.setSize(450,470);
 		frame.setLayout(new CardLayout());
-		//frame.add(pnlMainMenu,"Main Menu");
-		frame.add(pnlManageMusic,"Manage Music");
+		frame.add(pnlAddMusic, "Add Music");
+		frame.add(pnlMainMenu,"Main Menu");
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.setVisible(true);
 	}
-
-	private void createManageMusicScreen() {
-		lblManageMusic = new JLabel("Manage Music");
-		lblManageMusic.setFont(new Font("Comic Sans MS", Font.BOLD , 22));
-		lblManageMusic.setForeground(Color.MAGENTA);
+	/**
+	 * Creates the add music screen components
+	 */
+	private void createAddMusicScreen() {
+		//Create main panel 
+		pnlAddMusic = new JPanel();
+		pnlAddMusic.setLayout(new BorderLayout());
+		pnlAddMusic.add(lblAddMusic = new JLabel("Add Music"));
 		
-		// Add Music button
-		btnAddMusic = new JButton("Add Music");
+		//pnlAddMusic.add(lblAddMusic = new JLabel("Add Music"));
 		
-		// Create headings
-		String[] columnNames = {"Song Title",
-								"Artist",
-								"Album"};
-		
-		// Create sample data for testing, this will be pulled from the database in the real
-		// program
-		Object[][] sampleData = {
-			    {"Ssdfome Title", "Crappy Artist", "Stupid eeeAlbum"},
-			    {"Some Title", "Czxczxcvrappy Artist", "Stupvvixcvzxcvd Album"},
-			    {"Somebn Title", "Crapzxcvzxcpy Artist", "Stupidxcvzxcv Album"},
-			    {"Somevzxcvx Title", "Crappy 334Artist", "Stupid Album"},
-			    {"Somebvn Title", "Cdffrappy Artist", "Stupid vbnbAlbum"}
-		};
-		
-		JTable tblManageMusic = new JTable(sampleData, columnNames);
-		tblManageMusic.setEnabled(false);	// Disable editing for now
-		tblManageMusic.setAutoCreateRowSorter(true);
-		
-		// Create scroll pane to contain table
-		JScrollPane scrollPane = new JScrollPane(tblManageMusic);
-		tblManageMusic.setFillsViewportHeight(true);
-		
-		// Create panel to contain everything
-		pnlManageMusic = new JPanel();
-		pnlManageMusic.setLayout(new GridLayout(3,1));
-		pnlManageMusic.add(lblManageMusic);
-		pnlManageMusic.add(btnAddMusic);
-		pnlManageMusic.add(scrollPane);
 		
 	}
 
