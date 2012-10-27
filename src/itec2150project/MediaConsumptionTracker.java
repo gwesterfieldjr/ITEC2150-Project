3672228404eff3fa9e4859c17f5e00e53e8f31ba
Greg Westerfield, Jr. <gwesterfieldjr@gmail.com>
2012-10-27 11:08:56 -0400

    Test

--- src/itec2150project/MediaConsumptionTracker.java
+++ src/itec2150project/MediaConsumptionTracker.java
@@ -1,25 +1,40 @@
 package itec2150project;
 
+import java.awt.BorderLayout;
 import java.awt.CardLayout;
 import java.awt.GridLayout;
 import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.JTextField;
 
 /**
- * @author Raj Ramsaroop
+ * @author Raj Ramsaroop, Greg Westerfield, Jr.
  * @version 0.1
  */
 public class MediaConsumptionTracker {
-	//this is a chenge
+	
 	// The program window
 	JFrame frame;
 	
 	// Main Menu Panel
 	JPanel pnlMainMenu;
-	
+
 	// Main menu buttons
 	JButton btnMainMusic, btnMainMovies, btnMainBooks;
+	
+	//Add music panel
+	JPanel pnlAddMusic;
+	
+	//Add music buttons
+	JButton btnAddMusic, btnCancel;
+	
+	//Add music labels
+	JLabel lblAddMusic, lblSongTitle, lblArtist, lblAlbum;
+	
+	//Add music text fields 
+	JTextField txtSongTitle, txtArtist, txtAlbum;
 	
 	public MediaConsumptionTracker() {
 		
@@ -34,14 +49,29 @@ public class MediaConsumptionTracker {
 		
 		// Create main menu panel
 		createMainMenu();
-		
+		// Create add music panel
+		createAddMusicScreen();
 		// Create and initialize frame, make main menu the first panel displayed
 		frame = new JFrame("Media Consumption Tracker");
 		frame.setSize(450,470);
 		frame.setLayout(new CardLayout());
+		frame.add(pnlAddMusic, "Add Music");
 		frame.add(pnlMainMenu,"Main Menu");
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.setVisible(true);
+	}
+	/**
+	 * Creates the add music screen components
+	 */
+	private void createAddMusicScreen() {
+		//Create main panel 
+		pnlAddMusic = new JPanel();
+		pnlAddMusic.setLayout(new BorderLayout());
+		pnlAddMusic.add(lblAddMusic = new JLabel("Add Music"));
+		
+		//pnlAddMusic.add(lblAddMusic = new JLabel("Add Music"));
+		
+		
 	}
 
 	/**
